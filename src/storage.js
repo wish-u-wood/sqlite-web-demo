97484422f8e944691e5090d5073dde1738e2d831
Marcus Bevans <75916133+wish-u-wood@users.noreply.github.com>
2025-09-09 11:09:53 -0500

    Create storage.js

---
+++ src/storage.js
@@ -0,0 +1,23 @@
+<script>
+(function () {
+  var LS_KEY = "demo_sqlite_db_v1";
+
+  function saveDBBytes(u8) {
+    var s = "";
+    for (var i = 0; i < u8.length; i++) s += String.fromCharCode(u8[i]);
+    localStorage.setItem(LS_KEY, btoa(s));
+  }
+
+  function loadDBBytes() {
+    var b64 = localStorage.getItem(LS_KEY);
+    if (!b64) return null;
+    var bin = atob(b64);
+    var arr = new Uint8Array(bin.length);
+    for (var i = 0; i < bin.length; i++) arr[i] = bin.charCodeAt(i);
+    return arr;
+  }
+
+  window.App = window.App || {};
+  window.App.Storage = { saveDBBytes: saveDBBytes, loadDBBytes: loadDBBytes };
+})();
+</script>
