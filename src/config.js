49d8b57068b12ee462de586ce2fae383c4ad46c5
Marcus Bevans <75916133+wish-u-wood@users.noreply.github.com>
2025-09-09 11:22:23 -0500

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,4 @@
+window.App = window.App || {};
+window.App.Config = {
+  SQL_JS_CDN: "https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.10.2",
+  LS_KEY: "demo_sqlite_db_v2" // bump when schema changes
